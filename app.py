659c5b501bfaeca5d7c5a37b19d327842a5bcae4
Lais.ST <Laiscostast@gmail.com>
2021-08-19 23:58:05 +0000

    Final aula 2

--- app.py
+++ app.py
@@ -8,9 +8,23 @@ SECRET_KEY = 'pudim'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+#Funções de acesso ao banco de dados(obs: @app é o decorator)
 def conectar_bd():
     return sqlite3.connect(app.config['DATABASE'])
 
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def depois_request(exc):
+    g.bd.close()
+
+
+#Funções de rota
+@app.route('/')
+def exibir_entradas():
+    return render_template('exibir_entradas.html')
 
 @app.route('/hello')
 def pagina_inicial():
